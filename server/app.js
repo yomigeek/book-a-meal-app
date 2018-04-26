a5671b6f2f35bd4d5f910a8ac2d3f0d905f3dcc3
yomigeek <seayomi@gmail.com>
2018-04-26 13:13:58 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const bodyParser = require ('body-parser');
 const path = require ('path');
 const app = express();
 const port = process.env.PORT || 3000;
-const serve = app.listen(port);
 app.use(bodyParser.json());
 
 app.use(require('./routes/index'));
@@ -11,4 +10,3 @@ app.use(require('./routes/index'));
 app.listen(port , () => { console.log('Server started on port 3000'); });
 
 module.exports = app;
-module.exports = serve;
