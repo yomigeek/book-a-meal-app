4e0894543863d6c66aab81fb1b70116e4cd8e156
yomigeek <seayomi@gmail.com>
2018-04-26 14:19:45 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use(require('./routes/index'));
-
-app.listen(port , () => { console.log('Server started on port 3000'); });
+let servy = app.listen(port);
+console.log('Server started on port 3000');
 
 module.exports = app;
+module.exports = servy;
