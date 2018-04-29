d155c8f8f2ecedf2ec5e6d667e9e961e977bfd14
yomigeek <seayomi@gmail.com>
2018-04-29 22:59:07 +0100

    Update testApp.js

--- server/test/testApp.js
+++ server/test/testApp.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 
 const chaiHttp = require('chai-http');
 
-const server = require('../app');
+const server = require('../app').default;
 
 const should = chai.should();
 
