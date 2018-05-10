ccad4c59db33b635fe61b36066fa89551a6d92f7
Yomi Olaoye <seayomi@gmail.com>
2018-05-10 19:18:08 +0100

    Changes from local repository

--- server/auth.js
+++ server/auth.js
@@ -1,12 +1,13 @@
 import jwt from 'jsonwebtoken';
 import config from './config';
 
+require('dotenv').config();
 
 // This function verifies the token via the response header or body token
 export default function verifyToken(req, res, next) {
   const token = req.body.token || req.headers['x-access-token'];
   if (token) {
-    jwt.verify(token, config.secret, (error, decoded) => {
+    jwt.verify(token, process.env.SECRET, (error, decoded) => {
       if (error) {
         return res.status(401).send(error);
       }
--- server/db/myDb.js
+++ server/db/myDb.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
 
-const config = require('../config/config.json')[env];
-
+const config = require('../config/config')[env];
+  
 require('dotenv').config();
 
 
